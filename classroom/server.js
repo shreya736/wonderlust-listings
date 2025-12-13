89d540b18387335cc216f3fd7a46401cf321359e
Shreya Kadam <shreyakadam1305@gmail.com>
2025-12-13 08:47:06 -0800

    Update server.js

--- classroom/server.js
+++ classroom/server.js
@@ -2,16 +2,24 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
+const cookieParser = require("cookie-parser");
 
+app.use(cookieParser());
 
 app.get("/getcookies", (req, res) => {
     res.cookie("greet", "hello");
     res.cookie("greet", "namaste");
     res.cookie("madeIn", "India");
     res.send("sent you some cookies!");
+});
+
+app.get("/greet", (req, res) => {
+    let { name = "anonymous" } = req, cookies;
+    res.send(`Hi, ${name}`);
 })
 
 app.get("/", (req, res) => {
+    console.dir(req.cookies);
     res.send("Hi, I am root!");
 });
 
