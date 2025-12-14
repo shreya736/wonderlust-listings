36cdb58a5ef2b26882f40c3fdeaa43078443a283
Shreya Kadam <shreyakadam1305@gmail.com>
2025-12-14 08:26:11 -0800

    Update server.js

--- classroom/server.js
+++ classroom/server.js
@@ -2,29 +2,57 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
-const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const flash = require("connect-flash");
+const path = require("path");
 
-app.use(cookieParser());
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get("/getcookies", (req, res) => {
-    res.cookie("greet", "hello");
-    res.cookie("greet", "namaste");
-    res.cookie("madeIn", "India");
-    res.send("sent you some cookies!");
+const sessionOptions = {
+    secret: "mysupersecretstring",
+    resave: false,
+    saveUninitialized: true
+};
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.successMsg = req.flash("success");
+    res.locals.errorMsg = req.flash("error");
+    next();
 });
 
-app.get("/greet", (req, res) => {
-    let { name = "anonymous" } = req, cookies;
-    res.send(`Hi, ${name}`);
-})
-
-app.get("/", (req, res) => {
-    console.dir(req.cookies);
-    res.send("Hi, I am root!");
+app.get("/register", (req, res) => {
+    let { name = "anonymous" } = req.query;
+    req.session.name = name;
+    if (name === "anonymous") {
+        req.flash("error", "user not registered");
+    } else {
+        req.flash("success", "user registered successfully!");
+    }
+    res.redirect(`/hello`);
 });
 
-app.use("/users", users);
-app.use("/posts", posts);
+app.get("/hello", (req, res) => {
+    res.render("page.ejs", { name: req.session.name });
+});
+
+
+
+// app.get("/reqcount", (req, res) => {
+//     if (req.session.count) {
+//         req.session.count++;
+//     } else {
+//         req.session.count = 1;
+//     }
+//     res.send(`You sent a request ${req.session.count} times`);
+// });
+
+// app.get("/test", (req, res) => {
+//     res.send("test successful!");
+// });
 
 app.listen(3000, () => {
     console.log("server is listing to 3000");
