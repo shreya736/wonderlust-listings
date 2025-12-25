b542240dec433099bf887cb2d82a1329e2baab4c
Shreya Kadam <shreyakadam1305@gmail.com>
2025-12-25 00:16:21 -0800

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const flash = require("connect-flash");
 
 
 const listings = require("./routes/listing.js");
@@ -35,14 +36,24 @@ app.use(express.static(path.join(__dirname, "/public")));
 const sessionOptions = {
     secret: "mysupersecretcode",
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: true,
+    cookies: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+    },
 };
-
-app.use(session(sessionOptions));
-
 
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
+});
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    next();
 });
 
 
