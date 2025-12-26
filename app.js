8b03240de0a7853c8fae094495164f4444ba298b
Shreya Kadam <shreyakadam1305@gmail.com>
2025-12-26 07:40:23 -0800

    Update app.js

--- app.js
+++ app.js
@@ -7,76 +7,86 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
-
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
-main()
-    .then(() => {
-        console.log("connected to DB");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-
 async function main() {
     await mongoose.connect(MONGO_URL);
+    console.log("connected to DB");
 }
+main().catch(console.log);
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 const sessionOptions = {
     secret: "mysupersecretcode",
     resave: false,
     saveUninitialized: true,
-    cookies: {
-        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-        maxAge: 7 * 24 * 60 * 60 * 1000,
+    cookie: {
         httpOnly: true,
-    },
+        maxAge: 7 * 24 * 60 * 60 * 1000
+    }
 };
-
-app.get("/", (req, res) => {
-    res.send("Hi, I am root");
-});
 
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+// 🔥 Passport config (email login)
+passport.use(
+    new LocalStrategy(
+        { usernameField: "email" },
+        User.authenticate()
+    )
+);
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Flash middleware
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
 });
 
+// Test user route
+app.get("/demouser", async (req, res) => {
+    const fakeUser = new User({ email: "student@gmail.com" });
+    const registeredUser = await User.register(fakeUser, "helloworld");
+    res.send(registeredUser);
+});
 
+// Routes
 app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews);
 
-
-// app.all("*", (req, res, next) => {
-//     next(new ExpressError(404, "Page Not Found!"));
-// });
-
+// 404 handler
 app.use((req, res, next) => {
     next(new ExpressError(404, "Page Not Found!"));
 });
 
+// Error handler
 app.use((err, req, res, next) => {
-    let { statusCode = 500, message = "Something Went Wrong!" } = err;
+    const { statusCode = 500, message = "Something Went Wrong!" } = err;
     res.status(statusCode).render("error.ejs", { message });
-    // res.status(statusCode).send(message);
 });
-
 
 app.listen(8080, () => {
-    console.log("server is listening to port 8080");
+    console.log("server running on port 8080");
 });
+
