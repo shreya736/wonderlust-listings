36955500e7595a70311a728188cbafecbb69bdf3
Shreya Kadam <shreyakadam1305@gmail.com>
2025-12-29 23:41:22 -0800

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,8 +1,17 @@
 module.exports.isLoggedIn = (req, res, next) => {
-    console.log(req.user);
     if (!req.isAuthenticated()) {
-        req.flash("error", "you must be logged in to create listing!");
+        req.session.redirectUrl = req.originalUrl;
+        req.flash("error", "You must be logged in to create a listing!");
         return res.redirect("/login");
     }
     next();
 };
+
+module.exports.saveRedirectUrl = (req, res, next) => {
+    if (req.session.redirectUrl) {
+        res.locals.redirectUrl = req.session.redirectUrl;
+    }
+    next();
+};
+
+
