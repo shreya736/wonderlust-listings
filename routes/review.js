3ec44ce8a279478e7032eea0b7236f90cf14b781
Shreya Kadam <shreyakadam1305@gmail.com>
2025-12-30 22:48:50 -0800

    Update review.js

--- routes/review.js
+++ routes/review.js
@@ -2,19 +2,10 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
-const { reviewSchema } = require("../schema.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
+const { validateReview } = require("../middleware.js");
 
-const validateReview = (req, res, next) => {
-    let { error } = reviewSchema.validate(req.body);
-    if (error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    } else {
-        next();
-    }
-};
 
 //Post Review route
 router.post(
@@ -47,3 +38,4 @@ router.delete(
 );
 
 module.exports = router;
+
