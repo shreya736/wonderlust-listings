ab9665b1d7e1178ab47f9f375d16e49104b3202a
Shreya Kadam <shreyakadam1305@gmail.com>
2025-12-30 23:41:58 -0800

    Update review.js

--- routes/review.js
+++ routes/review.js
@@ -4,17 +4,19 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const { validateReview } = require("../middleware.js");
+const { validateReview, isLoggedIn } = require("../middleware.js");
 
 
 //Post Review route
 router.post(
     "/",
+    isLoggedIn,
     validateReview,
     wrapAsync(async (req, res) => {
         let listing = await Listing.findById(req.params.id);
         let newReview = new Review(req.body.review);
-
+        newReview.author = req.user._id;
+        console.log(newReview);
         listing.reviews.push(newReview);
 
         await newReview.save();
