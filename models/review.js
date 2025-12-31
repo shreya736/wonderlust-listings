11d83515381e8662094d4bbf23ac363758e376db
Shreya Kadam <shreyakadam1305@gmail.com>
2025-12-30 22:55:23 -0800

    Update review.js

--- models/review.js
+++ models/review.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+
 const reviewSchema = new Schema({
     Comment: String,
     rating: {
@@ -12,6 +13,11 @@ const reviewSchema = new Schema({
         type: Date,
         default: Date.now(),
     },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
 });
 
 module.exports = mongoose.model("Review", reviewSchema);
+
