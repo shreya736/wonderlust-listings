cb2811f97c07d2026ad43116a816eb47ada1bbc0
Shreya Kadam <shreyakadam1305@gmail.com>
2025-12-31 00:58:25 -0800

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 const Listing = require("./models/listing");
+const Review = require("./models/review");
 const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema, reviewSchema } = require("./schema.js");
 
@@ -51,4 +52,14 @@ module.exports.validateReview = (req, res, next) => {
     }
 };
 
+module.exports.isReviewAuthor = async (req, res, next) => {
+    let { id, reviewId } = req.params;
+    let review = await Review.findById(reviewId);
+    if (!review.author.equals(res.locals.currUser._id)) {
+        req.flash("error", "You are not the author of this review");
+        return res.redirect(`/listings/${id}`);
+    }
+
+    next();
+};
 
