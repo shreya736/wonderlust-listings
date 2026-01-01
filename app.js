8fd3af9f8cc8d59af6d4374c665bec930e275b7d
Shreya Kadam <shreyakadam1305@gmail.com>
2026-01-01 05:55:31 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+if (process.env.NODE_ENV != "production") {
+    require('dotenv').config();
+}
+
+
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -17,28 +23,34 @@ const userRouter = require("./routes/user.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
+main()
+    .then(() => {
+        console.log("connected to DB");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
 async function main() {
     await mongoose.connect(MONGO_URL);
-    console.log("connected to DB");
 }
-main().catch(console.log);
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.engine("ejs", ejsMate);
-
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname, "public")));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 const sessionOptions = {
     secret: "mysupersecretcode",
     resave: false,
     saveUninitialized: true,
     cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        maxAge: 7 * 24 * 60 * 60 * 1000
-    }
+    },
 };
 
 app.use(express.urlencoded({ extended: true }));
@@ -95,5 +107,6 @@ app.use((err, req, res, next) => {
 app.listen(8080, () => {
     console.log("server running on port 8080");
 });
+
 
 
