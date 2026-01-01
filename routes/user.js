6ae11c1bd0addaa79a4fb4f1f6dee5b021143eeb
Shreya Kadam <shreyakadam1305@gmail.com>
2026-01-01 02:11:28 -0800

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -7,31 +7,29 @@ const { saveRedirectUrl } = require("../middleware.js");
 
 const userController = require("../controllers/users.js");
 
-const { use } = require("./listing.js");
-
-router.get("/signup", userController.renderSigmupForm);
-
-router.post("/signup", wrapAsync(userController.signup));
+router.route("/signup")
+    .get(userController.renderSigmupForm)
+    .post(wrapAsync(userController.signup));
 
 
+router.route("/login")
+    .get(userController.renderLoginForm)
+    .post(
+        saveRedirectUrl,
+        passport.authenticate("local", {
+            failureRedirect: "/login",
+            failureFlash: true,
+        }),
+        userController.login
+    );
 
-router.get("/login", userController.renderLoginForm);
-
-router.post(
-    "/login",
-    saveRedirectUrl,
-    passport.authenticate("local", {
-        failureRedirect: "/login",
-        failureFlash: true,
-    }),
-    userController.login
-);
 
 
 
 router.get("/logout", userController.logout);
 
 module.exports = router;
+
 
 
 
