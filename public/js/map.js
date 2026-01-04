81d36a3b468ee9c01a3140eabac268025fad65d7
Shreya Kadam <shreyakadam1305@gmail.com>
2026-01-04 07:52:03 -0800

    Update map.js

--- public/js/map.js
+++ public/js/map.js
@@ -4,6 +4,16 @@ const map = new mapboxgl.Map({
     container: "map", // container ID
 
     style: "mapbox://styles/mapbox/streets-v12",
-    center: [77.209, 28.6139], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    center: [77.209006, 28.613895], // starting position [lng, lat]. Note that lat must be set between -90 and 90
     zoom: 9, // starting zoom
 });
+
+
+const marker = new mapboxgl.Marker({ color: "red" })
+    .setLngLat([77.209006, 28.613895]) //Listing.geometry.coordinates
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 }).setHTML(
+            `<h4>${listing.title}</h4><p>Exact Location will be provided after booking</p>`
+        )
+    )
+    .addTo(map);
